4a5e4b24e8420379b01f2485181fc74210559244
lakshmisailaja-cloud <bommureddy59@gmail.com>
2026-05-25 01:46:51 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,1 +1,20 @@
-print('Backend Server Running')
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(bytes("""
+<html>
+<body>
+<h1>Backend Running Successfully</h1>
+<h2>AWS DevOps 3 Tier Project</h2>
+</body>
+</html>
+""", "utf-8"))
+
+server = HTTPServer(("0.0.0.0", 5000), MyServer)
+print("Server running on port 5000")
+server.serve_forever()
